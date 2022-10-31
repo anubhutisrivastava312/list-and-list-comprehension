0ef3dbe77f5b276a8a652aee88681347b09150e1
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2022-10-31 17:32:33 +0530

    added 2 new programs

--- list_practice.py
+++ list_practice.py
@@ -10,6 +10,40 @@
 # print(" the second largest element is : ",max(list1))
 
 #program to print the second smallest number in a list
-list1=[2,5,89,90]
-list1.remove(min(list1))
-print(" the second smallest number is:",min(list1))
+# list1=[2,5,89,90]
+# list1.remove(min(list1))
+# print(" the second smallest number is:",min(list1))
+
+
+
+#program to find the sum of elements
+# list1=[2,3,1,4]
+# sum=0
+# for i in range(len(list1)):
+#     sum=sum+list1[i]
+# print(" the sum of elemnts is: ",sum)    
+
+
+
+#program to interchange the first and last elemnt of list
+# list1=[4,78,90,65,34,20]
+# temp=list1[0]
+# list1[0]=list1[-1]
+# list1[-1]=temp
+# print(" the list after: ",list1)
+
+
+
+#program to print the duplicate elements of list
+# list1=[9,9,86,54,54,12,89,86,20]
+# duplicate_list=[]
+# for i in list1:
+#     if i not in duplicate_list:
+#         duplicate_list.append(i)
+
+# print(" the duplicate elements are: ",duplicate_list)        
+
+
+
+
+
