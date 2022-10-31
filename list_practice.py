b2ec586867b19c6d1d86daf5b1064b4bb7a20f8a
Anubhuti Srivastava <anubhutisrivastava312@gmail.com>
2022-10-31 16:20:59 +0530

    added a new program

---
+++ list_practice.py
@@ -0,0 +1,15 @@
+#program to access the index of list 
+# list1=[12,45,77,90]
+# for i in range(len(list1)):
+#     value=list1[i]
+#     print(i,value)
+
+#program to print the second largest element in a list
+# list1=[5,10,87,90]
+# list1.remove(max(list1))
+# print(" the second largest element is : ",max(list1))
+
+#program to print the second smallest number in a list
+list1=[2,5,89,90]
+list1.remove(min(list1))
+print(" the second smallest number is:",min(list1))
